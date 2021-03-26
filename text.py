e52eaf3de05817ffc20a96ad9e1d97f457dc1931
orliLerner <orli.lerner@gmail.com>
2021-03-26 09:25:13 +0200

    first one

---
+++ text.py
@@ -0,0 +1,32 @@
+
+
+fname = input('Enter the file name: ')
+fh = open(fname)
+file = fh.read().split()
+ 
+def revword(word):
+   word = word.lower()[::-1]
+   return word
+
+def countword():
+    count = 1
+    word = file[0]
+    for i in file[1:]:
+        w = revword(i)
+        if w == word:
+            count = count + 1      
+    return count
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
